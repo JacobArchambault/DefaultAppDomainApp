3a785f403a9b0e43d3112d7bddacf39b552c5f9b
Jacob Archambault <jacob.archambault@waystar.com>
2020-02-28 15:54:50 -0500

    Clean up code.

--- Program.cs
+++ Program.cs
@@ -1,69 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Reflection;
 
 namespace DefaultAppDomainApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("***** Fun with the default app domain *****\n");
-            InitDAD();
-            DisplayDADStats();
-            ListAllAssembliesInAppDomain();
+            AppDomain defaultAppDomain = AppDomain.CurrentDomain;
+
+            InitializeDomain(defaultAppDomain);
+            DisplayStats(defaultAppDomain);
+            ListAllAssembliesInAppDomain(defaultAppDomain);
             Console.ReadLine();
         }
 
-        #region Display stats of default app domain
-        private static void DisplayDADStats()
+        private static void DisplayStats(AppDomain defaultAD)
         {
-            // Get access to the app domain for the current thread.
-
-            AppDomain defaultAD = AppDomain.CurrentDomain;
-
             // Print out various stats about this domain.
-            Console.WriteLine("Name of this domain: {0}", defaultAD.FriendlyName);
-            Console.WriteLine("ID of domain in this process: {0}", defaultAD.Id);
-            Console.WriteLine("Is this the default domain?: {0}", defaultAD.IsDefaultAppDomain());
-            Console.WriteLine("Base directory of this domain: {0}", defaultAD.BaseDirectory);
+            Console.WriteLine($"Name of this domain: {defaultAD.FriendlyName}");
+            Console.WriteLine($"ID of domain in this process: {defaultAD.Id}");
+            Console.WriteLine($"Is this the default domain?: {defaultAD.IsDefaultAppDomain()}");
+            Console.WriteLine($"Base directory of this domain: {defaultAD.BaseDirectory}");
         }
-        #endregion
 
-        #region List ASMS in app domain
-        static void ListAllAssembliesInAppDomain()
+        static void ListAllAssembliesInAppDomain(AppDomain defaultAD)
         {
-            // Get access to the app domain for the current thread.
-            AppDomain defaultAD = AppDomain.CurrentDomain;
-
             // Now get all loaded assemblies in the default app domain. 
             var loadedAssemblies = from a in defaultAD.GetAssemblies()
                                    orderby a.GetName().Name
                                    select a;
 
-            Console.WriteLine("***** Here are the assemblies loaded in {0} *****\n",
-                defaultAD.FriendlyName);
-            foreach (var a in loadedAssemblies)
+            Console.WriteLine($"***** Here are the assemblies loaded in {defaultAD.FriendlyName} *****\n");
+            foreach (Assembly a in loadedAssemblies)
             {
-                Console.WriteLine("-> Name: {0}", a.GetName().Name);
-                Console.WriteLine("-> Version: {0}\n", a.GetName().Version);
+                AssemblyName assemblyName = a.GetName();
+                Console.WriteLine($"-> Name: {assemblyName.Name}");
+                Console.WriteLine($"-> Version: {assemblyName.Version}\n");
             }
         }
-        #endregion
 
-        static void InitDAD()
+        static void InitializeDomain(AppDomain defaultAppDomain)
         {
             // This logic will print out the name of any assembly
-            // loaded into the applicaion domain, after it has been
+            // loaded into the application domain, after it has been
             // created. 
-            AppDomain defaultAD = AppDomain.CurrentDomain;
-            defaultAD.AssemblyLoad += (o, s) =>
-            {
-                Console.WriteLine("{0} has been loaded!", s.LoadedAssembly.GetName().Name);
-            };
+            defaultAppDomain.AssemblyLoad += (o, s) =>
+                Console.WriteLine($"{s.LoadedAssembly.GetName().Name} has been loaded!");
         }
     }
 }
